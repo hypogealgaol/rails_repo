941743fc2b7837f69a3cf973620a63c06ccb505c
Sinjihn Smith <sinjihnsmith@gmail.com>
2015-10-29 00:18:53 -0400

    did bcrypt password auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User
   field :first_name, type: String
   field :last_name, type: String
   field :email, type: String
-  field :password, type: String
+  field :password_digest, type: String
   field :password_confirmation, type: String
   
   
