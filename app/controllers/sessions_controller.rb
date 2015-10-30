f884a427a2ceb9d3ca6fc11082104669e814ffde
Sinjihn Smith <sinjihnsmith@gmail.com>
2015-10-30 15:53:19 -0400

    password_digest recent

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
 
   def create #login post
     puts "[SESSIONS_CONTROLLER] create"
-    user = User.find_by(email: params[:session][:email], password_digest: params[:session][:password])
+    user = User.find_by(email: params[:session][:email], password: params[:session][:password])
     if user && user.authenticate(params[:session][:password]) #authenticate the password
       puts 'logging in user'
       log_in user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
-class User
+require 'bcrypt'
+
+class User 
   include Mongoid::Document
   include ActiveModel::SecurePassword
 
@@ -9,12 +11,14 @@ class User
   field :first_name, type: String
   field :last_name, type: String
   field :email, type: String
-  field :password_digest, type: String
+  field :password, type: String
+  field :password_confirmation, type: String
   field :privilege, type: Boolean
   
   
-  #validates :password, :presence => { :message}
-  validates :password_digest, :confirmation =>true, :presence => true, length: {minimum: 6, maximum: 20, too_long: "%{count} characters is the maximum allowed"}
+  validates :password, :confirmation =>true, :presence => true, length: {minimum: 6, maximum: 20, too_long: "%{count} characters is the maximum allowed"}
+  validates_confirmation_of :password
+  validates :password_confirmation, :presence =>true
   validates :email, :presence => true, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
   validates_uniqueness_of :email
   validates :first_name, :presence => true
